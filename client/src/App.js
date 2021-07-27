318d16fc922baa7e94c86f0a18eea33283ea8d0b
Kaipo <kaipojames12@gmail.com>
2021-07-27 13:04:32 -0700

    Add material ui and basic front end display

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Character from './components/showCharacter/showCharacter.js'
+import CreateCharacter from './components/createCharacter/createCharacter.js'
 import './App.css';
+import useStyles from './style.js';
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Characters</Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appbar} position="static" color="inherit">
+                  <Character/>
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar className={classes.appbar} position="static" color="inherit">
+                  <CreateCharacter/>
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
