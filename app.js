b0bba7b3067ab6b0a49f0f14d404e62954106983
Kaipo <kaipojames12@gmail.com>
2021-08-01 09:54:17 -0700

    Modify deployment configurations to heroku

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 const app = express();
 const router = require('./config/routes/router');
+const PORT = process.end.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true })); // Enable our form data to be accessed by the 'req' variable in our routes
 app.use(express.json());
@@ -18,8 +19,13 @@ app.use(cors());
 
 app.use("/", router);
 
+// Check if our app is running on heroku
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 // CONNECT TO EXPRESS SERVER, THEN TO MongoDB
-app.listen(process.env.PORT || 5000, (err, res) => {
+app.listen(PORT, (err, res) => {
     if (err) console.log(err);
     console.log("Server is listening on port 5000 at: http://localhost:5000");
 
